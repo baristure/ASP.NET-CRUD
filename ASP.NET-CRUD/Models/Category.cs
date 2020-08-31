979b0fc3d09502463133c7a6879788ef02bd2399
Baris Ture <baristure@hotmail.com>
2020-08-31 23:56:38 +0300

    initial commit

---
+++ ASP.NET-CRUD/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CRUD.Models
+{
+    public class Category
+    {
+    }
+}
---
+++ ASP.NET-CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
