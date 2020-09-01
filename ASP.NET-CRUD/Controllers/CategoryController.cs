757c14d4072e27f17cce257bd1834e375f3b6c0b
Baris Ture <baristure@hotmail.com>
2020-09-01 23:36:41 +0300

    Category View page category list added

--- ASP.NET-CRUD/Controllers/CategoryController.cs
+++ ASP.NET-CRUD/Controllers/CategoryController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ASP.NET_CRUD.Data;
+using ASP.NET_CRUD.Models;
 
 namespace ASP.NET_CRUD.Controllers
 {
     public class CategoryController : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Categories;
+            return View(objList);
         }
     }
 }
