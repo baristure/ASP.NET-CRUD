05fda667b6c2a8262fe586dbef50560612a3229f
Baris Ture <baristure@hotmail.com>
2020-09-03 22:02:19 +0300

    Application Type MVC func added

--- ASP.NET-CRUD/Controllers/ApplicationTypeController.cs
+++ ASP.NET-CRUD/Controllers/ApplicationTypeController.cs
@@ -7,8 +7,8 @@ namespace ASP.NET_CRUD.Controllers
 {
     public class ApplicationTypeController : Controller
     {
-
         private readonly ApplicationDbContext _db;
+
         public ApplicationTypeController(ApplicationDbContext db)
         {
             _db = db;
@@ -22,22 +22,92 @@ namespace ASP.NET_CRUD.Controllers
         }
 
 
-        // GET - CREATE
+        //GET - CREATE
         public IActionResult Create()
         {
-
             return View();
         }
 
 
-        // POST - CREATE
+        //POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(ApplicationType obj)
         {
-            _db.ApplicationType.Add(obj);
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationType.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
+
+
         }
+
     }
 }
